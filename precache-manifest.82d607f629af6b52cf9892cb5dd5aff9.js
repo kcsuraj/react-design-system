92ff3089710c7c7475150bb141dc1d169b5692b4
kcsuraj <kcsuraj@users.noreply.github.com>
2020-05-30 15:16:48 +0000

    deploy: 16b74e52090b5799fd53916d1fb209f5c5a45189

---
+++ precache-manifest.82d607f629af6b52cf9892cb5dd5aff9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "587408152189d9b30e4e62837536a262",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.9f02d5982524c87a1a1c.bundle.js"
+  },
+  {
+    "url": "runtime~main.9f02d5982524c87a1a1c.bundle.js"
+  },
+  {
+    "url": "vendors~main.9f02d5982524c87a1a1c.bundle.js"
+  },
+  {
+    "url": "vendors~main.9f02d5982524c87a1a1c.bundle.js.LICENSE.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.30635043f8f42c675fe985bddba50a1d.js"
+  "precache-manifest.82d607f629af6b52cf9892cb5dd5aff9.js"
 );
 
 self.addEventListener('message', (event) => {
