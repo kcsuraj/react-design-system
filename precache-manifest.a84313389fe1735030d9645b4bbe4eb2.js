7bf12562e48d81b5ccf1060529db86b03f58cab7
kcsuraj <kcsuraj@users.noreply.github.com>
2020-05-30 10:13:15 +0000

    deploy: 37ec44815394ea3fd58b657aa1f3e441762a483c

---
+++ precache-manifest.a84313389fe1735030d9645b4bbe4eb2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62249e6b1edfc77015e7ff4f7aa0ec83",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.e252263481253b6db664.bundle.js"
+  },
+  {
+    "url": "runtime~main.e252263481253b6db664.bundle.js"
+  },
+  {
+    "url": "vendors~main.e252263481253b6db664.bundle.js"
+  },
+  {
+    "url": "vendors~main.e252263481253b6db664.bundle.js.LICENSE.txt"
+  }
+]);
