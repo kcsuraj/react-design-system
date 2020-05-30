36c93cb5335a3b38ab3e5d54d50095c1752e15b1
Suraj kc <isurajkc@gmail.com>
2020-05-30 23:17:52 +0545

    Add font sizes in theme and typographic scale in text stories

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -3,5 +3,6 @@ import { create } from '@storybook/theming/create';
 export default create({
   base: 'light',
   brandTitle: 'Design System ',
-  brandUrl: 'https://kcsuraj.github.io/react-design-system'
+  brandUrl: 'https://kcsuraj.github.io/react-design-system',
+  fontBase: '"Open Sans", sans-serif'
 });
