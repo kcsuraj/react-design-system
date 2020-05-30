f0e1909be2eddb2ebc92e4317bbfb4a0d3c72c17
Suraj kc <isurajkc@gmail.com>
2020-05-30 18:28:16 +0545

    Add mdx support for adding stories with addon blocks plugin

---
+++ .storybook/theme.js
@@ -0,0 +1,7 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Design System ',
+  brandUrl: 'https://kcsuraj.github.io/react-design-system'
+});
