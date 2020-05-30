cd351a6a8c8ddd8c331eb73e5e5e0e0948205b03
Suraj kc <isurajkc@gmail.com>
2020-05-30 15:46:10 +0545

    Updates

---
+++ precache-manifest.8f63f1b941a0355fbcc8cc1d9bf7d01b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e650702dc5f3826d3147a9239e41572",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.417ebe2262bc8a547849.bundle.js"
+  },
+  {
+    "url": "runtime~main.417ebe2262bc8a547849.bundle.js"
+  },
+  {
+    "url": "vendors~main.417ebe2262bc8a547849.bundle.js"
+  },
+  {
+    "url": "vendors~main.417ebe2262bc8a547849.bundle.js.LICENSE.txt"
+  }
+]);
