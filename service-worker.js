398b05538d7049710a50297e2327c7f4a0a6a8c9
kcsuraj <kcsuraj@users.noreply.github.com>
2020-07-16 03:43:00 +0000

    deploy: 9648e97ee7cd70a46d587cb1facb8672e780d757

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.d8a1907ad736a0835b88651f9b302f65.js"
+  "precache-manifest.0dabdff4762e25bcead84693969bd0ae.js"
 );
 
 self.addEventListener('message', (event) => {
